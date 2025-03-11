d8625e1ff66d971aacb2902e66e3e311c307e3e1
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-11 17:09:16 +0530

    webhook controller

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,46 @@
-import express from "express";
-import { createServer } from "http";
-import { initializeSocket } from "./socket";
-import dotenv from "dotenv";
+// import express from "express";
+// import { createServer } from "http";
+// import { initializeSocket } from "./socket";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const httpServer = createServer(app);
+// const app = express();
+// const httpServer = createServer(app);
 
-// Initialize socket.io
-const io = initializeSocket(httpServer);
+// // Initialize socket.io
+// const io = initializeSocket(httpServer);
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// httpServer.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+import "dotenv/config";
+import http from "http";
+import app from "./app";
+// import { connectDB } from "./config/db";
+
+import { socketService } from "./utils/socket";
+import { PORT } from "./config/env";
+
+const server = http.createServer(app);
+
+try {
+  socketService.initialize(server);
+  console.log("Socket.io initialized");
+} catch (error) {
+  console.log("Failed to initialize socket.io", error);
+  process.exit(1);
+}
+
+server.listen(PORT, async () => {
+  try {
+    // await connectDB();
+    console.log("Server is running on port :", PORT);
+  } catch (error) {
+    console.log("Failed to connect to DB", error);
+    process.exit(1);
+  }
 });
