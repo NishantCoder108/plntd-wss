cf9ea6e43967f36bef3b3b2138fabac5247215de
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-16 20:31:57 +0530

    comment console

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/webhook", removeConsole, authMiddleware, webhookRoutes);
+app.use("/webhook", authMiddleware, webhookRoutes);
 // app.use("/transaction", transactionRoutes);
 
 app.get("/test", (req, res) => {
